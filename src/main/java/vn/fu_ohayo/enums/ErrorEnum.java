749dffa81c7d65ff560da1a8a958f3e3071a3404
Nguyễn Trọng Đạt <124783881+AnhDat0103@users.noreply.github.com>
2025-06-08 13:41:16 +0700

    feat: implement lesson exercise management with CRUD operations and response handling

--- src/main/java/vn/fu_ohayo/enums/ErrorEnum.java
+++ src/main/java/vn/fu_ohayo/enums/ErrorEnum.java
@@ -49,7 +49,7 @@ public enum ErrorEnum {
     GRAMMAR_NOT_FOUND("4015", "Grammar not found with this title." ),
     CONTENT_READING_NOT_FOUND("4016", "Content reading not found"),
     VOCABULARY_ALREADY_EXISTS_IN_CONTENT_READING("4017","Vocabulary is  existed in content reading " ),
-    GRAMMAR_ALREADY_EXISTS_IN_CONTENT_READING("4018","Grammar is  existed in content reading " );
+    GRAMMAR_ALREADY_EXISTS_IN_CONTENT_READING("4018","Grammar is  existed in content reading " ),
     EXERCISE_QUESTION_NOT_FOUND("4019","No exercise questions found for this lesson exercise." );
 
 

e04a7aa7c1a7f8fdec1caf840e965d0fd968c74f
Nguyễn Trọng Đạt <124783881+AnhDat0103@users.noreply.github.com>
2025-06-10 08:48:21 +0700

    feat: refactor authentication and user role management for improved structure and efficiency

--- src/main/java/vn/fu_ohayo/service/JwtService.java
+++ src/main/java/vn/fu_ohayo/service/JwtService.java
@@ -5,6 +5,8 @@ import vn.fu_ohayo.dto.response.ExtractTokenResponse;
 import vn.fu_ohayo.enums.TokenType;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
 public interface JwtService {
     String generateAccessToken(Long userId, String email, Collection<? extends GrantedAuthority> authorities);

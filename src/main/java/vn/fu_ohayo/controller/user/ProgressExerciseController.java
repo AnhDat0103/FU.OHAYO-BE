936ea8fbe40da9e927c9e0f1529805f3a7895fa0
Y <datnthe180348@fpt.edu.vn>
2025-06-12 15:41:56 +0700

    feat(Progress): implement exercise submission and result tracking; add new DTOs and service methods for do exercise feature.

---
+++ src/main/java/vn/fu_ohayo/controller/user/ProgressExerciseController.java
@@ -0,0 +1,31 @@
+package vn.fu_ohayo.controller.user;
+
+import org.springframework.web.bind.annotation.*;
+import vn.fu_ohayo.dto.request.UserResponseRequest;
+import vn.fu_ohayo.dto.response.ApiResponse;
+import vn.fu_ohayo.dto.response.ExerciseResultResponse;
+import vn.fu_ohayo.service.ProgressExerciseService;
+
+@RestController
+@RequestMapping("/do-exercises")
+public class ProgressExerciseController {
+
+    private final ProgressExerciseService progressExerciseService;
+    public ProgressExerciseController(ProgressExerciseService progressExerciseService) {
+        this.progressExerciseService = progressExerciseService;
+    }
+
+    @PostMapping
+    public ApiResponse<ExerciseResultResponse> submit(
+            @RequestBody UserResponseRequest userResponseRequest
+    ) {
+        ExerciseResultResponse result = progressExerciseService.submitExercise(userResponseRequest);
+
+        return ApiResponse.<ExerciseResultResponse>builder()
+                .code("200")
+                .status("success")
+                .message("Exercise submitted successfully")
+                .data(result)
+                .build();
+    }
+}

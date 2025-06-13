e21e435c808e73beb2364e7ccfa34dd046c61c34
Nguyen Van An <annvhe181169@gpt.edu.vn>
2025-06-13 12:08:29 +0700

    feat(ParentStudent): add endpoints for learning progress and user information retrieval

--- src/main/java/vn/fu_ohayo/controller/user/ParentStudentController.java
+++ src/main/java/vn/fu_ohayo/controller/user/ParentStudentController.java
@@ -6,11 +6,17 @@ import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import vn.fu_ohayo.dto.request.CodeRequest;
 import vn.fu_ohayo.dto.response.ApiResponse;
 import vn.fu_ohayo.dto.response.ApprovalResponse;
-import vn.fu_ohayo.service.ParentStudentService;
+import vn.fu_ohayo.dto.response.LearningProgressOverviewResponse;
+import vn.fu_ohayo.dto.response.UserResponse;
+import vn.fu_ohayo.mapper.UserMapper;
+import vn.fu_ohayo.service.*;
 
 @RestController()
 @RequestMapping("/parent-student")
@@ -19,6 +25,11 @@ import vn.fu_ohayo.service.ParentStudentService;
 @Slf4j(topic = "ParenController")
 public class ParentStudentController {
    ParentStudentService parentStudentService;
+    ExerciseResultService exerciseResultService;
+    ProgressGrammarService progressGrammarService;
+    ProgressVocabularyService progressVocabularyService;
+    UserService userService;
+    UserMapper userMapper;
 //    @Autowired
 //    SimpMessagingTemplate messa;
 
@@ -70,6 +81,67 @@ public class ParentStudentController {
                 .build();
     }
 
+    @GetMapping("/student/overview")
+    ApiResponse<LearningProgressOverviewResponse> getLearningProgressResponse(
+                @RequestParam long userId
+    ) {
+        LearningProgressOverviewResponse response = LearningProgressOverviewResponse.builder()
+                .totalVocabularyAllSubject(progressVocabularyService.countAllVocabularySubjectInProgressByUserId(userId))
+                .totalVocabularyLearn(progressVocabularyService.countVocabularyLearnSubjectInProgressByUserId(userId))
+                .exerciseAllSubject(exerciseResultService.countAllExerciseSubjectInProgressByUserId(userId))
+                .exerciseCompleted(exerciseResultService.countExerciseDoneSubjectInProgressByUserId(userId))
+                .totalGrammarAllSubject(progressVocabularyService.countAllVocabularySubjectInProgressByUserId(userId))
+                .totalGrammarLearn(progressGrammarService.countGrammarLearnSubjectInProgressByUserId(userId))
+                .build();
+        return ApiResponse.<LearningProgressOverviewResponse>builder()
+                .status("success")
+                .message("Fetched all lessons successfully")
+                .data(response)
+                .build();
+    }
+
+    @GetMapping("/student/vocabulary")
+    ApiResponse<?> getProgressVocabularyByUserId(
+            @RequestParam long userId
+    ) {
+        return ApiResponse.<Object>builder()
+                .status("success")
+                .message("Fetched vocabulary progress successfully")
+                .data(progressVocabularyService.getProgressEachSubjectByUserId(userId))
+                .build();
+    }
+
+    @GetMapping("/student/grammar")
+    ApiResponse<?> getProgressGrammarByUserId(
+            @RequestParam long userId
+    ) {
+        return ApiResponse.<Object>builder()
+                .status("success")
+                .message("Fetched grammar progress successfully")
+                .data(progressGrammarService.getProgressEachSubjectByUserId(userId))
+                .build();
+    }
+
+    @GetMapping("/student/exercise")
+    ApiResponse<?> getProgressExerciseByUserId(
+            @RequestParam long userId
+    ) {
+        return ApiResponse.<Object>builder()
+                .status("success")
+                .message("Fetched exercise progress successfully")
+                .data(exerciseResultService.getProgressEachSubjectByUserId(userId))
+                .build();
+    }
+    @GetMapping("/student/information")
+    public ApiResponse<?> getInformationByUserId(@RequestParam long userId
+    ) {
+             UserResponse userResponse = userMapper.toUserResponse(userService.getUserById(userId));
+        return ApiResponse.<UserResponse>builder()
+                .status("success")
+                .message("Fetched user information successfully")
+                .data(userResponse)
+                .build();
+    }
 
 }
 

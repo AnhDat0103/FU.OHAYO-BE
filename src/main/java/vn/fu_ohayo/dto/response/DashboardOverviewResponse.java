b1c250addc6b7f8737b4b87fbe725172910862ae
Nguyen Van An <annvhe181169@gpt.edu.vn>
2025-06-20 10:54:58 +0700

    feat(Response): add response DTOs for dashboard and user activity trends

---
+++ src/main/java/vn/fu_ohayo/dto/response/DashboardOverviewResponse.java
@@ -0,0 +1,29 @@
+package vn.fu_ohayo.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import vn.fu_ohayo.entity.SystemLog;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class DashboardOverviewResponse {
+    int totalUserNow;
+    int totalUserLastMonth;
+    int totalSubjectNow;
+    int totalSubjectLastMonth;
+    int totalLessonNow;
+    int totalLessonLastMonth;
+    double averageCompletionSubjectNow;
+    double averageCompletionSubjectLastMonth;
+    int totalUserNormal;
+    int totalUserOneYear;
+    int totalUserOneMonth;
+    int totalUserSixMonths;
+    List<SystemLog> systemLogs;
+}
---
+++ src/main/java/vn/fu_ohayo/service/impl/DashboardAdminServiceImp.java
@@ -0,0 +1,7 @@
+package vn.fu_ohayo.service.impl;
+
+import org.springframework.stereotype.Service;
+import vn.fu_ohayo.service.DashboardAdminService;
+@Service
+public class DashboardAdminServiceImp implements DashboardAdminService {
+}

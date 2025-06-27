fc599385b53c9d47a18e84628606dd3df4288770
Nguyễn Trọng Đạt <124783881+AnhDat0103@users.noreply.github.com>
2025-06-28 00:32:05 +0700

    feat(Grammar): add methods to manage grammars in lessons, including retrieval and deletion

--- src/main/java/vn/fu_ohayo/repository/GrammarRepository.java
+++ src/main/java/vn/fu_ohayo/repository/GrammarRepository.java
@@ -1,13 +1,16 @@
 package vn.fu_ohayo.repository;
+import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import vn.fu_ohayo.entity.Grammar;
 import vn.fu_ohayo.entity.Lesson;
+import vn.fu_ohayo.entity.Vocabulary;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,4 +26,22 @@ public interface GrammarRepository extends JpaRepository<Grammar, Integer> {
             "WHERE l.lessonId = :lessonId AND g.deleted = false")
     Page<Grammar> findAllByLessonIdAndDeletedIsFalse(@Param("lessonId") int lessonId, Pageable pageable);
 
+    @Transactional
+    @Modifying
+    @Query(
+            value = "DELETE FROM lesson_grammar where grammar_id = :id AND lesson_id = :lessonId", nativeQuery = true
+    )
+    void removeGrammarInLessonId(int id, int lessonId);
+
+    @Query("SELECT g FROM Grammar g WHERE g.deleted = false AND g.grammarId NOT IN " +
+            "(SELECT gl.grammarId  FROM  Lesson l  JOIN l.grammars gl WHERE l.lessonId = :lessonId)")
+    Page<Grammar> findAllNotInLesson(int lessonId, Pageable pageable);
+
+
+    @Transactional
+    @Modifying
+    @Query(
+            value= "INSERT INTO lesson_grammar (lesson_id, grammar_id) VALUES (:lessonId, :grammarId)", nativeQuery = true
+    )
+    void saveGrammarIntoLesson(int lessonId, int grammarId);
 }

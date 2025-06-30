0896b50affe2ad03736bf6815942acc3e94383c9
Nguyễn Trọng Đạt <124783881+AnhDat0103@users.noreply.github.com>
2025-07-01 00:37:39 +0700

    feat(Progress): add functionality to mark progress subject as complete and update related lessons

--- src/main/java/vn/fu_ohayo/repository/ProgressLessonRepository.java
+++ src/main/java/vn/fu_ohayo/repository/ProgressLessonRepository.java
@@ -6,7 +6,12 @@ import vn.fu_ohayo.entity.Lesson;
 import vn.fu_ohayo.entity.ProgressLesson;
 import vn.fu_ohayo.entity.User;
 
+import java.util.Collection;
+import java.util.List;
+
 @Repository
 public interface ProgressLessonRepository extends JpaRepository<ProgressLesson, Integer> {
     ProgressLesson findByUserAndLesson(User user, Lesson lesson);
+
+    List<ProgressLesson> findAllByUserAndLessonIn(User user, List<Lesson> lessons);
 }

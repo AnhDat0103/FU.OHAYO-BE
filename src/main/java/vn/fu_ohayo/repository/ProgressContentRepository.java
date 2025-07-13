e37f8d294ab108f0cb9b6c332b63f6c39041e125
huydmhe180961 <huydmhe180961@fpt.edu.vn>
2025-07-13 16:26:51 +0700

    feat(ContentReadingProgress): add endpoint to retrieve completed articles; enhance ContentListeningService with status validation

--- src/main/java/vn/fu_ohayo/repository/ProgressContentRepository.java
+++ src/main/java/vn/fu_ohayo/repository/ProgressContentRepository.java
@@ -5,6 +5,7 @@ import vn.fu_ohayo.entity.Content;
 import vn.fu_ohayo.entity.ContentReading;
 import vn.fu_ohayo.entity.ProgressContent;
 import vn.fu_ohayo.entity.User;
+import vn.fu_ohayo.enums.ProgressStatus;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,4 +15,7 @@ public interface ProgressContentRepository extends JpaRepository<ProgressContent
     Optional<ProgressContent> findByUser_UserIdAndContent_ContentId(Long userId, Long contentId);
     Optional<ProgressContent> findByUserAndContent(User user, Content content);
 //    Optional<ProgressContent> findByUser_UserAndContent_Content(Long userId, Long contentId);
+    List<ProgressContent> findByUser_UserIdAndProgressStatus(int userId, ProgressStatus progressStatus);
+
+    User user(User user);
 }
